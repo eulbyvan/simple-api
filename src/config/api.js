41e8361911ffdedeffd0191a2fc4e3531279c723
eulbyvan <work.issdq@gmail.com>
2023-01-03 09:42:57 +0700

    02-config

---
+++ src/config/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+/*
+ * Author : Stu (https://www.eulbyvan.com)
+ * Created on : Tue Jan 03 2023 09:40:13
+ * Copyright (c) 2023
+ */
+
+const axiosInstance = axios.create({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+    headers: {
+        "Content-type": "application/json"
+    }
+});
+
+export default axiosInstance;
