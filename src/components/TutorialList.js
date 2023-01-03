8a28c627b6ee431c3a465c06cf0da03db03c11b9
eulbyvan <work.issdq@gmail.com>
2023-01-03 09:46:39 +0700

    Create TutorialList.js

---
+++ src/components/TutorialList.js
@@ -0,0 +1,28 @@
+/*
+ * Author : Stu (https://www.eulbyvan.com)
+ * Created on : Tue Jan 03 2023 09:43:37
+ * Copyright (c) 2023
+ */
+
+const TutorialList = ({ data }) => {
+    <table>
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>description</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data?.map((item, index) => (
+                <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.description}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>;
+};
+
+export default TutorialList;
